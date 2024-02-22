d59150edfc9a9cae4af0342ca74b0a1cba93e4b0
er2427 <elise.raschke@marquette.edu>
2024-02-22 08:04:19 -0600

    Pushing these changes because i have to reclone

--- app/src/main/java/com/example/clubsportsappnew/MainActivity.java
+++ app/src/main/java/com/example/clubsportsappnew/MainActivity.java
@@ -1,12 +1,10 @@
 package com.example.clubsportsappnew;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.Menu;
 
-import com.example.clubsportsappnew.ui.home.Filter;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
