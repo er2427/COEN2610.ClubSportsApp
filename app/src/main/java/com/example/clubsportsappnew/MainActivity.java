4cddedabb79a25ea154672678ee29b400b4b313f
bekkah-83 <rebekkah.scufsa@marquette.edu>
2024-03-08 01:13:59 -0600

    Login Page added in and starts up the page on

--- app/src/main/java/com/example/clubsportsappnew/MainActivity.java
+++ app/src/main/java/com/example/clubsportsappnew/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.clubsportsappnew;
 
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.Menu;
+import android.content.Intent;
 
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
@@ -23,10 +26,17 @@ public class MainActivity extends AppCompatActivity {
     private AppBarConfiguration mAppBarConfiguration;
     private ActivityMainBinding binding;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        if (!isLoggedIn()) {
+            // Start LoginPage activity
+            Intent intent = new Intent(this, com.example.clubsportsappnew.ui.home.LoginPage.class);
+            startActivity(intent);
+            finish();
+            return;
+        }
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
@@ -86,6 +96,11 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
+    }
+
+    public boolean isLoggedIn() {
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        return sharedPreferences.getBoolean("isLoggedIn", false);
     }
 }
 
