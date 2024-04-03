073098f15bc66462bf5d02d83b3a2c383e828fd1
er2427 <elise.raschke@marquette.edu>
2024-04-03 11:38:10 -0500

    Working calendar yay!

--- app/src/main/java/com/example/clubsportsappnew/ui/calendar/CalendarFragment.java
+++ app/src/main/java/com/example/clubsportsappnew/ui/calendar/CalendarFragment.java
@@ -5,21 +5,55 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CalendarView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.widget.SearchView;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.clubsportsappnew.R;
+import com.example.clubsportsappnew.VersionsAdapter;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 
 public class CalendarFragment extends Fragment {
 
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_calendar, container, false); //get the layout from the xml
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        CalendarView calendarView = view.findViewById(R.id.calendarView);
+
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+
+                // This is where you'll display events based on date
+
+
+                // Use Calendar class to create a Date object
+                Calendar calendar = Calendar.getInstance();
+                calendar.set(year, month, dayOfMonth);
+                Date date = calendar.getTime();
+
+                // Display the date in a Toast
+                Toast.makeText(requireContext(), date.toString(), Toast.LENGTH_SHORT).show();
+            }
+        }); // Add closing parenthesis and semicolon here
+    }
 
 
 }
