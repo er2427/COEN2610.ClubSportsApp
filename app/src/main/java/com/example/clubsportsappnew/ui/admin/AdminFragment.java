3af460ff09f8d5933f3843d70bb8f56d102bcb92
KaylaLoly <kayla.imanzi@marquette.edu>
2024-04-19 11:38:49 -0500

    Admin framework Updated

--- app/src/main/java/com/example/clubsportsappnew/ui/admin/AdminFragment.java
+++ app/src/main/java/com/example/clubsportsappnew/ui/admin/AdminFragment.java
@@ -3,25 +3,42 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.database.Cursor;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
+
+import com.example.clubsportsappnew.R;
+import com.example.clubsportsappnew.ui.calendar.EventAdapter;
 
 import com.example.clubsportsappnew.ui.home.DatabaseHelper;
 
 public class AdminFragment extends AppCompatActivity {
 
     private DatabaseHelper databaseHelper;
+    private SQLiteDatabase getReadableDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_admin)
+        setContentView(R.layout.fragment_admin);
         databaseHelper = new DatabaseHelper(this);
 
-        String username;
-        boolean isAdmin = databaseHelper.isAdminUser(username);
+        //To be finalized when I have the context from database admin column
+        //String username = ;
+        //boolean isAdmin = databaseHelper.finalize(username);
+        public boolean isAdminUser(String username) {
+            SQLiteDatabase db = this.getReadableDatabase;
+            boolean isAdmin = false;
 
+            try{
+                Cursor cursor = db.rawQuery(query, new String[]{username});
+            }
+
+        }
         if (isAdmin) {
             Toast.makeText(this, "Admin user detected", Toast.LENGTH_SHORT).show();
             //Handle add event
